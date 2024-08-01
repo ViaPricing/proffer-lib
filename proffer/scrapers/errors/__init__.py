b16f15b0976a5ecbeba52a7ec9993534924dc436
Lucas Machado <lucasmachado.rj@gmail.com>
2024-08-01 11:11:06 -0300

    add message property

--- proffer/scrapers/errors/__init__.py
+++ proffer/scrapers/errors/__init__.py
@@ -1,6 +1,8 @@
 class CustomError(Exception):
     """Base class for other exceptions"""
-    pass
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
 
 class EmptyResponseError(CustomError):
     """Raised when the response is empty"""
