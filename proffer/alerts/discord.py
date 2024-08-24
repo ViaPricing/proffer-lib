5b6a87d851ae8dc30b75262892e368c03900498a
Bernardo Chede <bechede@id.uff.br>
2024-08-24 10:23:06 -0400

    changed embeded discord alert message

--- proffer/alerts/discord.py
+++ proffer/alerts/discord.py
@@ -1,20 +1,34 @@
 import os
 import requests
+from datetime import datetime
 
 class Discord:
     
-    def __init__(self):
-        discord_token = os.getenv("DISCORD_TOKEN")
-        discord_bot_id = os.getenv("DISCORD_BOT_ID")
-        self.webhook_url = f"https://discord.com/api/webhooks/{discord_bot_id}/{discord_token}"
-        if not discord_token or not discord_bot_id:
+    def __init__(self, token, bot_id):
+        self.webhook_url = f"https://discord.com/api/webhooks/{bot_id}/{token}"
+        if not token or not bot_id:
             print("Discord token or bot id not found in environment variables")
     
     def send_alert(self, message: str, error, metadata):
         formated_msg = f"{message}: {str(error)}, {metadata}"
+        date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        
         payload = {
-            "content": formated_msg
+            "embeds": [
+                {
+                    "title": "Token Inválido",
+                    "description": formated_msg,
+                    "fields": [
+                        {"name": "Scraper", "value": str(metadata.get('scraper_name', 'N/A')), "inline": True},
+                        {"name": "Date", "value": date_time, "inline": True},
+                        {"name": "Token", "value": str(metadata.get('token', 'N/A')), "inline": True},
+                        {"name": "Message", "value": formated_msg, "inline": False}
+                    ],
+                    "color": 15158332 
+                }
+            ]
         }
+        
         try:
             response = requests.post(self.webhook_url, json=payload)
             if not response.ok:
@@ -23,5 +37,7 @@ class Discord:
             print(f"Failed to send alert to Discord: {str(e)}")
             
 
-def discord_bot():
-    return Discord()
+def get_discord_bot():
+    token = os.getenv("DISCORD_TOKEN")
+    bot_id = os.getenv("DISCORD_BOT_ID")
+    return Discord(token, bot_id)
